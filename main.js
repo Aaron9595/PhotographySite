d571fb09db3669c328cadfb4fb6b4754747dce88
Aaron <aaron.nelson1995@gmail.com>
2018-10-24 05:41:04 -0600

    working on modal images in portfolio

--- main.js
+++ main.js
@@ -26,3 +26,21 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+
+// var modal = document.getElementById("myModal");
+
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("modal-caption");
+// img.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+
+// var span = document.getElementsByClassName("close")[0];
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
